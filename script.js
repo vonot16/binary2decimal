6a093759e951e8bae9158d123612daed4f539d6f
vonot16 <rafaelfeliperm@gmail.com>
2022-06-04 04:14:50 -0300

    added KeyDown Event Listener

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.onload = () =>{
     [binaryInput, decimalInput] = document.getElementsByTagName('input')
+    binaryInput.addEventListener('keydown',binaryChangeHandler)
+}
+
+function binaryChangeHandler(e){
     
 }
