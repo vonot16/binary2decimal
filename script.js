1979c9473c70b6c7ac8a77272bcd4672cee1f35b
vonot16 <rafaelfeliperm@gmail.com>
2022-06-04 04:51:15 -0300

    cancelInputChange Function Finished

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ window.onload = () =>{
 }
 
 function binaryChangeHandler(e){
-    if(parseInt(e.key)==1 || parseInt(e.key)==0)
-        console.log("Valid Number")
+    if(parseInt(e.key)!=1 && parseInt(e.key)!=0 && e.key!="Enter")
+        setTimeout(() => {cancelInputChange(e.key)},20)
     else if(e.key=="Enter")
         console.log("send")
-    else console.log("Invalid Number")
+}
+
+function cancelInputChange(key){
+    binaryInput.value = binaryInput.value.replace(key,"")
 }
