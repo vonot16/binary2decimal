f1304543645840ef8fc93bfeebd51b51e7e41873
vonot16 <rafaelfeliperm@gmail.com>
2022-06-04 04:57:29 -0300

    getButton and Convert Function Started

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 window.onload = () =>{
     [binaryInput, decimalInput] = document.getElementsByTagName('input')
+    const convertButton = document.getElementsByTagName('button')[0]
     binaryInput.addEventListener('keydown',binaryChangeHandler)
+    convertButton.addEventListener('click',convert)
 }
 
 function binaryChangeHandler(e){
     if(parseInt(e.key)!=1 && parseInt(e.key)!=0 && e.key!="Enter")
         setTimeout(() => {cancelInputChange(e.key)},20)
     else if(e.key=="Enter")
-        console.log("send")
+        convert()
 }
 
 function cancelInputChange(key){
     binaryInput.value = binaryInput.value.replace(key,"")
+}
+
+function convert(){
+    let binary = [...binaryInput.value]
+    console.log(binary)
 }
